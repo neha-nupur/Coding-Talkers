20e813da9e7f4d07804f48a929a3991615fef6d4
Neha <nehanupur497@gmail.com>
2023-05-21 17:28:27 +0530

    Updated index.js

--- index.php
+++ index.php
@@ -20,7 +20,6 @@ if (isset($_SESSION["user_id"])) {
 <head>
     <title>Home</title>
     <meta charset="UTF-8">
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"> -->
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
--- login.php
+++ login.php
@@ -38,7 +38,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <title>Login</title>
     <meta charset="UTF-8">
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"> -->
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
