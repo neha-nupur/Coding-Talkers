66172c3ede81a4ec7d7edfa339ab02c781ceb8c5
Neha <nehanupur497@gmail.com>
2023-05-23 10:17:49 +0530

    ...

--- index.php
+++ index.php
@@ -18,25 +18,28 @@ if (isset($_SESSION["user_id"])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home</title>
+    <title>Confirmation</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    
+    <div class="confirm">
+
     <h1 style="color:#fff;">Home</h1>
     
     <?php if (isset($user)): ?>
         
         <p style="color:#fff;">Hello <?= htmlspecialchars($user["name"]) ?></p>
-        
+        <p style="color:#fff;">Congrats! You are now Logged In</p> 
         <p><a href="logout.php">Log out</a></p>
         
     <?php else: ?>
         
         <p><a href="login.php">Log in</a> or <a href="signup.html">sign up</a></p>
         
-    <?php endif; ?>
+    <?php endif; ?> <p><a href="index.html">Go To Home Page</a></p>
+    </div>
+    
     
 </body>
 </html>
